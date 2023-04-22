241e915b38c23327bb28a66a49556e35a34bdfa0
aravindsak <aravindk.ark@gmail.com>
2023-04-22 14:59:11 +0530

    testing api

--- src/main/java/com/spirngcrud/crudapp/SpringcrudApplication.java
+++ src/main/java/com/spirngcrud/crudapp/SpringcrudApplication.java
@@ -2,10 +2,17 @@ package com.spirngcrud.crudapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringcrudApplication {
 
+	@GetMapping("/hello")
+	public String hello(){
+		return "springboot rest-api" ;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcrudApplication.class, args);
 	}
