ff88b6c60891cd54956f47cbc7e0f52108882c80
root <aravindk.ark@gmail.com>
2023-04-23 21:54:42 +0530

    add data

--- src/main/java/com/spirngcrud/crudapp/SpringcrudApplication.java
+++ src/main/java/com/spirngcrud/crudapp/SpringcrudApplication.java
@@ -1,7 +1,11 @@
 package com.spirngcrud.crudapp;
 
+import com.spirngcrud.crudapp.item.Item;
+import com.spirngcrud.crudapp.item.ItemRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +21,19 @@ public class SpringcrudApplication {
 		SpringApplication.run(SpringcrudApplication.class, args);
 	}
 
+	 @Bean
+	public CommandLineRunner commandLineRunner(
+			ItemRepository repository
+
+	) {
+		 return args -> {
+			 var item = Item.builder()
+					 .name("samsung")
+					 .details("M series")
+					 .build();
+			 repository.insert(item);
+
+
+		 };
+	 }
 }
